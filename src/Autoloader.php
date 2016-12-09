8063167f674f849c1a26c2650d2ee448194642db
kuati <brunokuati@gmail.com>
2016-12-09 18:59:55 -0200

    Update

--- src/Autoloader.php
+++ src/Autoloader.php
@@ -1,14 +1,30 @@
 <?php
 
+/**
+ * @todo Autoloader class
+ * @author: kuati <brunokuati@gmail.com>
+ * @license: MIT 
+ * @version 1.0
+ */
 class AutoLoader {
 
    private  static $directorys = Array();
 
+    /**
+     * @todo Autoloader config funtion
+     * @param $directorys as array of directorys
+     */
     public static function config(Array $directorys) : AutoLoader {
+        if (is_array($directorys)) {
+            throw new Exception("Argument error", 1);
+        }
         self::$directorys = $directorys;
         return new static;
     }
 
+    /**
+     * @todo Autoload register function
+     */
     public static function register() {
         spl_autoload_register(function ($class) {
             foreach(self::$directorys as $directory) {
